9bb05ccfc4ba1507132c1b8195f628fb054af889
Adrian Waddell <waddell.adrian@gene.com>
2022-10-24 13:11:48 -0700

    add exercise 19 & 20

--- exercise_19.R
+++ exercise_19.R
@@ -1,9 +1,143 @@
 
-library(rtables)
 
 # Group summaries
 
+library(rtables)
+library(dplyr)
+
+ex_adsl3 <- ex_adsl |>
+  mutate(B1HL = factor(ifelse(BMRKR1 > mean(BMRKR1), "H", "L"), levels = c("L", "H")))
+
 # Stage 1
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  split_rows_by("SEX") |> 
+  split_rows_by("B1HL") |>
+  analyze("AGE", \(x) list(B = "-"))
+
+build_table(lyt, ex_adsl3)
 
 # Stage 2
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  summarize_row_groups(cfun = function(x, labelstr) {
+    lst <- setNames(
+      list(rcell("r", format = "xx")),
+      labelstr
+    )
+    in_rows(.list = lst)
+  }) |>
+  split_rows_by("SEX") |> 
+  split_rows_by("B1HL") |>
+  analyze("AGE", \(x) list(B = "a"))
 
+build_table(lyt, ex_adsl3)
+
+# Stage 3
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups(cfun = function(x, labelstr) {
+    lst <- setNames(
+      list(rcell("s", format = "xx")),
+      labelstr
+    )
+    in_rows(.list = lst)
+  }) |>
+  split_rows_by("B1HL") |>
+  analyze("AGE", \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+# Stage 4
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups(cfun = function(x, labelstr) {
+    lst <- setNames(
+      list(rcell("s", format = "xx")),
+      labelstr
+    )
+    in_rows(.list = lst)
+  }) |>
+  split_rows_by("B1HL") |>
+  analyze("AGE", \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+# Stage 5
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  split_rows_by("SEX") |> 
+  split_rows_by("B1HL") |>
+  summarize_row_groups(cfun = function(x, labelstr) {
+    lst <- setNames(
+      list(rcell("bm", format = "xx")),
+      labelstr
+    )
+    in_rows(.list = lst)
+  }) |>
+  analyze("AGE", \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+# Now all together
+foo <- function(str) {
+  function(x, labelstr) {
+    lst <- setNames(
+      list(rcell(str, format = "xx")),
+      labelstr
+    )
+    in_rows(.list = lst)
+  }
+}
+
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  summarize_row_groups(cfun = foo("r")) |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups(cfun = foo("s")) |>
+  split_rows_by("B1HL") |>
+  summarize_row_groups(cfun = foo("bm")) |>
+  analyze("AGE", afun = \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+
+# Lets look at the default group summary
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  summarize_row_groups() |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups() |>
+  split_rows_by("B1HL") |>
+  summarize_row_groups() |>
+  analyze("AGE", afun = \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+
+# Fix the row label for the root summary
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  summarize_row_groups(label_fstr = "all") |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups() |>
+  split_rows_by("B1HL") |>
+  summarize_row_groups() |>
+  analyze("AGE", afun = \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
+
+
+# Add summary to the row label
+lyt <- basic_table() |>
+  split_cols_by("ARM") |>
+  summarize_row_groups(label_fstr = "summary (all)") |>
+  split_rows_by("SEX") |> 
+  summarize_row_groups(label_fstr = "summary %s") |>
+  split_rows_by("B1HL") |>
+  summarize_row_groups(label_fstr = "summary %s") |>
+  analyze("AGE", afun = \(x) list(B = "a"))
+
+build_table(lyt, ex_adsl3)
--- exercise_20.R
+++ exercise_20.R
@@ -2,6 +2,15 @@
 # Create an adverse events table
 
 library(rtables)
+library(dplyr)
+
+head(ex_adae[, c("USUBJID", "AEBODSYS", "AEDECOD")])
+
+ex_adae2 <- ex_adae |>
+  mutate(
+    AEBODSYS = sub("cl", "sys", AEBODSYS),
+    AEDECOD = sub("dcd", "code", AEDECOD)
+  )
 
 s_events_patients <- function(x, labelstr, .N_col) {
   in_rows(
@@ -15,13 +24,23 @@ s_events_patients <- function(x, labelstr, .N_col) {
 s_events_patients(x = c("id 1", "id 1", "id 2"), .N_col = 5)
 
 lyt <- basic_table() %>%
-  split_cols_by("ARM") %>%
+  split_cols_by("ARM", split_fun = add_overall_level("All Patients")) %>%
   add_colcounts() %>%
+  summarize_row_groups("USUBJID", cfun = s_events_patients) %>%
   split_rows_by("AEBODSYS", child_labels = "visible", indent_mod = 1)  %>%
   summarize_row_groups("USUBJID", cfun = s_events_patients) %>%
   analyze("AEDECOD", afun = list_wrap_x(table), indent_mod = -1)
 
-tbl <- build_table(ex_adae, alt_counts_df = ex_adsl)
+tbl <- build_table(lyt, ex_adae2, alt_counts_df = ex_adsl)
 
 tbl
-tbl1
+
+## TODO: fix indenting
+
+## remove (prune) rows with all zero counts
+
+## TODO
+
+## sort highest count
+
+# TODO
