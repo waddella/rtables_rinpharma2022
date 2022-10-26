4dc87a5490297446b6531fe59a7d5caea9a92050
Gabriel Becker <gabembecker@gmail.com>
2022-10-25 20:02:56 -0700

    push changes to exercise 4

--- exercise_04.R
+++ exercise_04.R
@@ -6,27 +6,25 @@
 
 
 # Save as txt file, \n>>>>\n page separator
-export_as_txt(tbl, file = "./text_output_01.txt", page_type = "a4", paginate = TRUE,
+export_as_txt(tbl, file = "./text_output_01.txt", page_type = "a4", font_size = 12, paginate = TRUE,
               page_break = "\n>>>>\n")
 
-export_as_txt(tbl, file = "./text_output_02.txt", page_type = "a4", paginate = TRUE,
+widthexport_as_txt(tbl, file = "./text_output_02.txt", page_type = "a4", paginate = TRUE,
               landscape = TRUE, page_break = "\n>>>>\n")
 
 
 # Save as pdf (note one pdf with multiple pages)
 
-export_as_txt(tbl, file = "./pdf_output_01.pdf", page_type = "a4", paginate = TRUE,
-              page_break = "\n>>>>\n")
+export_as_pdf(tbl, file = "./pdf_output_01.pdf", page_type = "a4", paginate = TRUE)
 
 export_as_pdf(tbl, file = "./pdf_output_02.pdf", page_type = "a4", paginate = TRUE,
-              landscape = TRUE, page_break = "\n>>>>\n")
+              landscape = TRUE)
 
 
 # Save as RTF
 
-export_as_rtf(tbl, file = "./rtf_output_01.rtf", page_type = "a4", paginate = TRUE,
-              page_break = "\n>>>>\n")
+export_as_rtf(tbl, file = "./rtf_output_01.rtf", page_type = "a4")
 
-export_as_rtf(tbl, file = "./rtf_output_02.rtf", page_type = "a4", paginate = TRUE,
-              landscape = TRUE, page_break = "\n>>>>\n")
+export_as_rtf(tbl, file = "./rtf_output_02.rtf", page_type = "a4",
+              landscape = TRUE)
 
